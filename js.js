71e2292692b0e283002c22a7618367015af629c6
Deep SatyaVikas <92770363+satyavikasgubbala@users.noreply.github.com>
2023-11-06 20:32:28 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+console.log("Hello World!");
+
+
+//method 1st of object creation
+var person = {
+    student :"vikas",
+    age : 23,
+    qualification : "B.Tech"
+}
+
+// console.log(person);
+// console.log(person.age);
+
+//method 2nd object creation
+
+var student = new Object();
+student.name="Vikas";
+student.age = 23;
+// console.log(student);
+
+console.log(student.name);
+console.log(student['age']);
